5c6e7d94acef38a85944daed1d9671df1dc5ceb9
Ouafae Haddouchi <oehaddouchi@Gmail.com>
2018-01-11 23:24:00 -0500

    update

--- ajax/user.php
+++ ajax/user.php
@@ -108,37 +108,7 @@
   elseif($action =="reset"){
         $email=$_POST['email'];
 
-        //send the email
-        
-        require_once('../lib/phpmailer/class.phpmailer.php');
-
-        $mail = new PHPMailer;
-
-        $mail->IsSMTP();                                      // Set mailer to use SMTP
-        $mail->Host = 'smtp.gmail.com';                 // Specify main and backup server
-        $mail->Port = 587;                                    // Set the SMTP port
-        $mail->SMTPAuth = true;                               // Enable SMTP authentication
-        $mail->Username = 'mytodolistwebapp@gmail.com';                // SMTP username
-        $mail->Password = 'abcd4321';                  // SMTP password
-        $mail->SMTPSecure = 'tls';                            // Enable encryption, 'ssl' also accepted
-
-        $mail->From = 'mytodolistwebapp@gmail.com';
-        $mail->FromName = 'MyToDoList';// Add a recipient
-        $mail->AddAddress('oehaddouchi@gmail.com');               // Name is optional
-
-        $mail->IsHTML(true);                                  // Set email format to HTML
-
-        $mail->Subject = 'This is your email reseting email';
-        $mail->Body    = 'This is the HTML message body <strong>in bold!</strong>';
-        $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
-
-        if(!$mail->Send()) {
-        echo 'Message could not be sent.';
-        echo 'Mailer Error: ' . $mail->ErrorInfo;
-        exit;
-        }
-
-        echo 'Message has been sent';
+        //send out email reset
   }
   $stmt=null;
 ?>
